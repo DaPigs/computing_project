5dead279eb58267163ef548a7ddd95b20ce7e07b
DaPigs <41998991+DaPigs@users.noreply.github.com>
2023-03-29 10:54:33 +0800

    added time.sleep

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 choice = input("Hello! CRUD or WebApp?").lower()
 if(choice == "crud"):
-    from crud import main
-    main()
+  from crud import main
+  main()
 elif(choice == "webapp"):
-    from webapp import app
-    app.run(host="0.0.0.0", port=8080)
+	from webapp import app
+	print("\033[93mOpen this link in a new window, if not this project won't work.\nhttps://database-project.zhang-yezhouyez.repl.co/\033[0m")
+	import time
+	time.sleep(2)
+	app.run(host="0.0.0.0", port=8080)
